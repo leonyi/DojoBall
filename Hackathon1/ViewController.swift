22c909f8a2fb5246982488a6dd29c681b11d9430
LisaRyland <lisa.ryland@gmail.com>
2018-01-15 16:50:19 -0800

    added swipe motion

--- Hackathon1/ViewController.swift
+++ Hackathon1/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 import CoreMotion
 
+//category array
+var categoryArr = ["Algorithms", "Uplifting Quotes", "Jokes"]
+
+//main content arrays
 var algoArr = [
-    "How would you verify a prime number?",
+    "Beginning: How would you verify a prime number?",
     "How could you find all prime factors of a number?",
     "How do get nth Fibonacci number?",
     "How would you find the greatest common divisor of two numbers?",
-    "How would you remove duplicate members from an array?"
+    "Ending: How would you remove duplicate members from an array?"
 ]
-var shakeCount = 0
+var inspirationalQuoteArr = ["inspo1", "inspo2", "inspo3", "inspo4", "inspo5"]
+var jokeArr = ["joke1", "joke2", "joke3", "joke4", "joke5"]
 
-var categoryArr = ["Algorithms", "Uplifting Quotes", "Jokes"]
-var categoryShakeCount = 0
+//shake counters
+var categorySwipeCount = 0
+var mainContentShakeCount = 0
+
 
 var motionManager: CMMotionManager?
 
@@ -35,7 +42,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let ownView: UIView = self.view
         
+        //when app first loads
+        categoryLabel.text = categoryArr[0]
+        stringLabel.text = algoArr[0]
+        
+        //detecting device motion
         if motionManager.isDeviceMotionAvailable {
             print("We can detect device motion")
             startReadingMotionData()
@@ -44,6 +57,20 @@ class ViewController: UIViewController {
             print("We cannot detect device motion")
         }
         
+        //swipe gestures
+        let leftSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeGesture))
+        let rightSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeGesture))
+        
+        leftSwipeGesture.direction = .left
+        rightSwipeGesture.direction = .right
+        
+        ownView.addGestureRecognizer(leftSwipeGesture)
+        ownView.addGestureRecognizer(rightSwipeGesture)
+        //        iterateMainContent()
+    }
+    
+    func iterateMainContent() {
+        //when device motion is available
         if motionManager.isDeviceMotionAvailable {
             motionManager.deviceMotionUpdateInterval = 0.02
             motionManager.startDeviceMotionUpdates(to: .main) {
@@ -51,38 +78,77 @@ class ViewController: UIViewController {
                 if let x = data?.userAcceleration.x,
                     x < -2.5 {
                     self?.navigationController?.popViewController(animated: true)
-                    self?.categoryLabel.text = categoryArr[categoryShakeCount]
                     
-                    if categoryShakeCount == categoryArr.count - 1 {
-                        categoryShakeCount  = 0
-                        self?.stringLabel.text = ""
+                    //when shake occurs
+                    if mainContentShakeCount == algoArr.count - 1 {
+                        mainContentShakeCount  = 0
+                        self?.stringLabel.text = algoArr[mainContentShakeCount]
                     }
                     else {
-                        print("Motion ended")
-                        self?.stringLabel.text = categoryArr[categoryShakeCount]
-                        categoryShakeCount += 1
+                        self?.stringLabel.text = algoArr[mainContentShakeCount]
+                        mainContentShakeCount += 1
                     }
-//                    categoryShakeCount += 1
-                    self?.stringLabel.text = ""
+                    mainContentShakeCount += 1
                 }
             }
         }
     }
     
+    @objc func swipeGesture(sender: UISwipeGestureRecognizer) {
+        // go to next category
+        if(sender.direction == .right) {
+            print("swiped right")
+            if categorySwipeCount == categoryArr.count - 1 {
+                categorySwipeCount = 0
+                categoryLabel.text = categoryArr[categorySwipeCount]
+            }
+            else {
+                categorySwipeCount += 1
+                categoryLabel.text = categoryArr[categorySwipeCount]
+            }
+        }
+        
+        // go to previous
+        if(sender.direction == .left) {
+            print("Swiping left")
+            print(categorySwipeCount)
+            if categorySwipeCount == 0 {
+                categorySwipeCount = categoryArr.count - 1
+                categoryLabel.text = categoryArr[categorySwipeCount]
+                print(categoryArr[categorySwipeCount])
+            }
+            else {
+                categorySwipeCount -= 1
+                categoryLabel.text = categoryArr[categorySwipeCount]
+                print(categoryArr[categorySwipeCount])
+            }
+        }
+    }
+    
+    
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
         print("Motion started")
+        
+        
+        
     }
     
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
-        if shakeCount == algoArr.count - 1 {
-            shakeCount = 0
-            stringLabel.text = algoArr[shakeCount]
+        
+        
+        
+        if mainContentShakeCount == algoArr.count - 1 {
+            mainContentShakeCount = 0
+            stringLabel.text = algoArr[mainContentShakeCount]
         }
         else {
             print("Motion ended")
-            stringLabel.text = algoArr[shakeCount]
-            shakeCount += 1
+            stringLabel.text = algoArr[mainContentShakeCount]
+            mainContentShakeCount += 1
         }
+        
+        
+        
     }
     
     func startReadingMotionData() {
@@ -102,6 +168,6 @@ class ViewController: UIViewController {
     func degrees(_ radians: Double) -> Double {
         return 180/Double.pi * radians
     }
-
+    
 }
 
